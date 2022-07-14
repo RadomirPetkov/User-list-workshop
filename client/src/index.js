0b51dade0a17d940d80155a488074c453c95304f
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2022-07-14 21:44:31 +0300

    initialization of project

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+<script src="https://kit.fontawesome.com/3b7d954735.js" crossorigin="anonymous"></script>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ 
+ <div>Users</div>
+);
