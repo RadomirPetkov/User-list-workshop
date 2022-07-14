181724e757fbcf34a234c42b3d0041221dcaadf3
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2022-07-14 22:09:57 +0300

    add search and users components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import { Header } from "./components/common/Header";
 import "./App.css"
 import { Footer } from "./components/common/Footer";
+import { Search } from "./components/search/Search";
+import { UserList } from "./components/user-list-table/UserList";
 
 function App() {
   return (
     <div>
       <Header />
       <main className="main">
-
-
+        <section className="card users-container">
+          <Search />
+          <UserList />
+        </section>
       </main>
       <Footer />
     </div>
